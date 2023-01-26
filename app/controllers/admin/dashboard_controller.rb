307c007086b4fe6308e2058f275451190cc00465
Hadley <sutherlandhadley@gmail.com>
2023-01-26 02:11:20 -0700

    add dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret"
+  # http_basic_authenticate_with name: "a", password:"a"
+  # name: ENV["HTTP_ADMIN_USER_NAME"],
+  # password: ENV["HTTP_BASIC_PASSWORD"]
   def show
+    @products_count = Product.all.length
+    @categories_count = Category.all.length
+    @orders_count = Order.all.length
+    @line_items_count = LineItem.all.length
   end
 end
